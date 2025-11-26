fe51f173cb1c69ec7b78210f3acfb4365cfaf5f7
angelaandrango <angela.andrango@yachaytech.edu.ec>
2025-11-26 18:39:55 -0500

    Add files via upload

---
+++ LAB09/Mini_Grammar.pl
@@ -0,0 +1,23 @@
+sentence --> noun_phrase, verb_phrase.
+
+noun_phrase --> determiner, adjectives, noun.
+verb_phrase --> verb, noun_phrase.
+
+determiner --> [the].
+determiner --> [a].
+
+noun --> [cat].
+noun --> [dog].
+noun --> [fish].
+noun --> [bird].
+
+verb --> [eats].
+verb --> [sees].
+
+adjective --> [big].
+adjective --> [small].
+adjective --> [angry].
+
+adjectives --> [].
+adjectives --> adjective, adjectives.
+
